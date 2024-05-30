d6a6c8913c7893fa93cbdaa0788b1c030399fbc3
Pavithra-S1 <pavithra.s@refex.co.in>
2024-05-30 12:32:04 +0530

    testing

--- visitor-management-backend/server.js
+++ visitor-management-backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
+// Middlewareuiiii
 app.use(cors());
 app.use(bodyParser.json());
 
